ac13c2b4840a97b405b220b5c0738a8c12bc96de
John Stableford <john.stableford@latitudefinancial.com>
2019-08-17 22:24:30 +1000

    refactor: use gobin to respect go modules

--- internal/test/client/doc.go
+++ internal/test/client/doc.go
@@ -1,4 +1,4 @@
 package client
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --package=client -o client.gen.go client.yaml
+//go:generate gobin -m -run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --package=client -o client.gen.go client.yaml
 
