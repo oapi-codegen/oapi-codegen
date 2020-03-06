10a42a593f7308a129f59068f3725964665319d8
qystishere <qystishere@gmail.com>
2020-03-06 15:22:52 +0800

    go generate

--- internal/test/components/components.gen.go
+++ internal/test/components/components.gen.go
@@ -94,7 +94,9 @@ type ParamsWithAddPropsParams_P1 struct {
 type ParamsWithAddPropsParams struct {
 
 	// This parameter has additional properties
-	P1 ParamsWithAddPropsParams_P1 `json:"p1"`
+	P1 struct {
+		AdditionalProperties map[string]interface{} `json:"-"`
+	} `json:"p1"`
 
 	// This parameter has an anonymous inner property which needs to be
 	// turned into a proper type for additionalProperties to work
