4f949756f314c6e8b86b78490622ceed3e50ac34
Simon Rüegg <simon@rueggs.ch>
2020-05-07 23:56:10 +0200

    Update security provider function signature (#175)
    * Update SecurityProvider function signatures
    
    In #148 the signatures were updated to follow go standards.
    
    Signed-off-by: Simon Rüegg <simon@rueggs.ch>
    
    * Wirte tests for SecurityProviders
    
    Signed-off-by: Simon Rüegg <simon@rueggs.ch>

---
+++ pkg/securityprovider/securityprovider_test.go
@@ -0,0 +1,42 @@
+package securityprovider
+
+import (
+	"testing"
+
+	"github.com/deepmap/oapi-codegen/internal/test/client"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	withTrailingSlash string = "https://my-api.com/some-base-url/v1/"
+)
+
+func TestSecurityProviders(t *testing.T) {
+	bearer, err := NewSecurityProviderBearerToken("mytoken")
+	assert.NoError(t, err)
+	client1, err := client.NewClient(
+		withTrailingSlash,
+		client.WithRequestEditorFn(bearer.Intercept),
+	)
+	assert.NoError(t, err)
+
+	apiKey, err := NewSecurityProviderApiKey("cookie", "apikey", "mykey")
+	assert.NoError(t, err)
+	client2, err := client.NewClient(
+		withTrailingSlash,
+		client.WithRequestEditorFn(apiKey.Intercept),
+	)
+	assert.NoError(t, err)
+
+	basicAuth, err := NewSecurityProviderBasicAuth("username", "password")
+	assert.NoError(t, err)
+	client3, err := client.NewClient(
+		withTrailingSlash,
+		client.WithRequestEditorFn(basicAuth.Intercept),
+	)
+	assert.NoError(t, err)
+
+	assert.Equal(t, withTrailingSlash, client1.Server)
+	assert.Equal(t, withTrailingSlash, client2.Server)
+	assert.Equal(t, withTrailingSlash, client3.Server)
+}
