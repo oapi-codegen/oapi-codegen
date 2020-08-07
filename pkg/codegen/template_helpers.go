f082bd4820677b423a019cdf2a3cbefbf9fb0ca1
Natsu Kagami <natsukagami@gmail.com>
2020-08-07 16:30:20 -0400

    Add statusText

--- pkg/codegen/template_helpers.go
+++ pkg/codegen/template_helpers.go
@@ -16,7 +16,9 @@ package codegen
 import (
 	"bytes"
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"text/template"
 
@@ -277,4 +279,11 @@ var TemplateFunctions = template.FuncMap{
 	"lower":                      strings.ToLower,
 	"title":                      strings.Title,
 	"stripNewLines":              stripNewLines,
+	"statusText": func(s string) (string, error) {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			return "", err
+		}
+		return http.StatusText(v), nil
+	},
 }
