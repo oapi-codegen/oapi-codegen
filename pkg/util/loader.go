85b487ce101b71122630c8441fb3d236ee365263
James Eagle <jimeagle@gmail.com>
2020-08-13 13:01:42 +0100

    Detect non local paths

--- pkg/util/loader.go
+++ pkg/util/loader.go
@@ -1,13 +1,20 @@
 package util
 
 import (
+	"net/url"
+
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
 func LoadSwagger(filePath string) (swagger *openapi3.Swagger, err error) {
+
 	loader := openapi3.NewSwaggerLoader()
 	loader.IsExternalRefsAllowed = true
-	swagger, err = loader.LoadSwaggerFromFile(filePath)
-	return
-}
 
+	u, err := url.Parse(filePath)
+	if err == nil && u.Scheme != "" && u.Host != "" {
+		return loader.LoadSwaggerFromURI(u)
+	} else {
+		return loader.LoadSwaggerFromFile(filePath)
+	}
+}
