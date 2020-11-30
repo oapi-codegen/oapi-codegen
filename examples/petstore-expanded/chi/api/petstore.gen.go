80b2992490e817e7baf74da5aad13f15ed631f19
Maciej Bratek <maciej.bratek@karhoo.com>
2020-11-30 13:43:16 +0100

    Allow for server middlewares in chi server

--- examples/petstore-expanded/chi/api/petstore.gen.go
+++ examples/petstore-expanded/chi/api/petstore.gen.go
@@ -80,8 +80,11 @@ type ServerInterface interface {
 
 // ServerInterfaceWrapper converts contexts to parameters.
 type ServerInterfaceWrapper struct {
-	Handler ServerInterface
+	Handler            ServerInterface
+	HandlerMiddlewares []middlewareFunc
 }
+
+type middlewareFunc func(http.HandlerFunc) http.HandlerFunc
 
 // FindPets operation middleware
 func (siw *ServerInterfaceWrapper) FindPets(w http.ResponseWriter, r *http.Request) {
@@ -114,14 +117,30 @@ func (siw *ServerInterfaceWrapper) FindPets(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	siw.Handler.FindPets(w, r.WithContext(ctx), params)
+	var handler = func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.FindPets(w, r, params)
+	}
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler(w, r.WithContext(ctx))
 }
 
 // AddPet operation middleware
 func (siw *ServerInterfaceWrapper) AddPet(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	siw.Handler.AddPet(w, r.WithContext(ctx))
+	var handler = func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.AddPet(w, r)
+	}
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler(w, r.WithContext(ctx))
 }
 
 // DeletePet operation middleware
@@ -139,7 +158,15 @@ func (siw *ServerInterfaceWrapper) DeletePet(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	siw.Handler.DeletePet(w, r.WithContext(ctx), id)
+	var handler = func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.DeletePet(w, r, id)
+	}
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler(w, r.WithContext(ctx))
 }
 
 // FindPetById operation middleware
@@ -157,35 +184,65 @@ func (siw *ServerInterfaceWrapper) FindPetById(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	siw.Handler.FindPetById(w, r.WithContext(ctx), id)
+	var handler = func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.FindPetById(w, r, id)
+	}
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler(w, r.WithContext(ctx))
 }
 
 // Handler creates http.Handler with routing matching OpenAPI spec.
 func Handler(si ServerInterface) http.Handler {
-	return HandlerFromMux(si, chi.NewRouter())
+	return HandlerWithOptions(si, ChiServerOptions{})
+}
+
+type ChiServerOptions struct {
+	BaseURL     string
+	BaseRouter  chi.Router
+	Middlewares []middlewareFunc
 }
 
 // HandlerFromMux creates http.Handler with routing matching OpenAPI spec based on the provided mux.
 func HandlerFromMux(si ServerInterface, r chi.Router) http.Handler {
-	return HandlerFromMuxWithBaseURL(si, r, "")
+	return HandlerWithOptions(si, ChiServerOptions{
+		BaseRouter: r,
+	})
 }
 
 func HandlerFromMuxWithBaseURL(si ServerInterface, r chi.Router, baseURL string) http.Handler {
+	return HandlerWithOptions(si, ChiServerOptions{
+		BaseURL:    baseURL,
+		BaseRouter: r,
+	})
+}
+
+// HandlerWithOptions creates http.Handler with additional options
+func HandlerWithOptions(si ServerInterface, options ChiServerOptions) http.Handler {
+	r := options.BaseRouter
+
+	if r == nil {
+		r = chi.NewRouter()
+	}
 	wrapper := ServerInterfaceWrapper{
-		Handler: si,
+		Handler:            si,
+		HandlerMiddlewares: options.Middlewares,
 	}
 
 	r.Group(func(r chi.Router) {
-		r.Get(baseURL+"/pets", wrapper.FindPets)
+		r.Get(options.BaseURL+"/pets", wrapper.FindPets)
 	})
 	r.Group(func(r chi.Router) {
-		r.Post(baseURL+"/pets", wrapper.AddPet)
+		r.Post(options.BaseURL+"/pets", wrapper.AddPet)
 	})
 	r.Group(func(r chi.Router) {
-		r.Delete(baseURL+"/pets/{id}", wrapper.DeletePet)
+		r.Delete(options.BaseURL+"/pets/{id}", wrapper.DeletePet)
 	})
 	r.Group(func(r chi.Router) {
-		r.Get(baseURL+"/pets/{id}", wrapper.FindPetById)
+		r.Get(options.BaseURL+"/pets/{id}", wrapper.FindPetById)
 	})
 
 	return r
