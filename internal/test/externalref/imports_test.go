b9a7fa2e7f6a4f15956b7b6825ead6eab99c218a
Alexey Dudko <dududko@gmail.com>
2021-03-26 23:41:11 +0100

    test "GetSwagger" function using the example with external references

--- internal/test/externalref/imports_test.go
+++ internal/test/externalref/imports_test.go
@@ -3,6 +3,8 @@ package externalref
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/deepmap/oapi-codegen/internal/test/externalref/packageA"
 	"github.com/deepmap/oapi-codegen/internal/test/externalref/packageB"
 )
@@ -13,4 +15,15 @@ func TestParameters(t *testing.T) {
 		ObjectA: &packageA.ObjectA{ObjectB: b},
 		ObjectB: b,
 	}
+}
+
+func TestGetSwagger(t *testing.T) {
+	_, err := packageB.GetSwagger()
+	require.Nil(t, err)
+
+	_, err = packageA.GetSwagger()
+	require.Nil(t, err)
+
+	_, err = GetSwagger()
+	require.Nil(t, err)
 }
