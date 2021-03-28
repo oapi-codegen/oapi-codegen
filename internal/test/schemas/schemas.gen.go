f7be070498de62e774799637d33320c4f5e05b03
Mark Rösler <marcodroesler@gmail.com>
2021-03-28 20:24:19 +0200

    fix tests

--- internal/test/schemas/schemas.gen.go
+++ internal/test/schemas/schemas.gen.go
@@ -28,13 +28,13 @@ const (
 	Access_tokenScopes = "access_token.Scopes"
 )
 
-// N5StartsWithNumber represents This schema name starts with a number.
+// This schema name starts with a number.
 type N5StartsWithNumber map[string]interface{}
 
 // AnyType1 defines model for AnyType1.
 type AnyType1 interface{}
 
-// AnyType2 represents This should be an interface{}.
+// This should be an interface{}.
 type AnyType2 interface{}
 
 // CustomStringType defines model for CustomStringType.
