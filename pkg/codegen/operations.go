124f782576c62f0d9f5103bedfd4965965e53a65
Weidong Feng <weidong_feng@qq.com>
2021-04-03 05:15:50 +0800

    feature(extension): support x-omitempty (#295)
    Co-authored-by: Weidong Feng <weidong.feng@shanbay.com>
    Co-authored-by: Marcin Romaszewicz <47459980+deepmap-marcinr@users.noreply.github.com>

--- pkg/codegen/operations.go
+++ pkg/codegen/operations.go
@@ -596,10 +596,11 @@ func GenerateParamsTypes(op OperationDefinition) []TypeDefinition {
 			})
 		}
 		prop := Property{
-			Description:   param.Spec.Description,
-			JsonFieldName: param.ParamName,
-			Required:      param.Required,
-			Schema:        pSchema,
+			Description:    param.Spec.Description,
+			JsonFieldName:  param.ParamName,
+			Required:       param.Required,
+			Schema:         pSchema,
+			ExtensionProps: &param.Spec.ExtensionProps,
 		}
 		s.Properties = append(s.Properties, prop)
 	}
