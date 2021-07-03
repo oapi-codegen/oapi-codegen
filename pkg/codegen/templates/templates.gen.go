5ff85d507e365a849098180fff001f893b68141f
Johan <jl311888@gmail.com>
2021-07-03 12:14:19 +0200

    remove unnecessary model import

--- pkg/codegen/templates/templates.gen.go
+++ pkg/codegen/templates/templates.gen.go
@@ -748,7 +748,7 @@ import (
 	"strings"
 	"time"
 
-	. {{.ModelPackageName}}
+	{{if eq .ModelPackageName "github.com/lingio//models"}} . {{.ModelPackageName}}{{end}}
 	"github.com/deepmap/oapi-codegen/pkg/runtime"
 	openapi_types "github.com/deepmap/oapi-codegen/pkg/types"
 	"github.com/getkin/kin-openapi/openapi3"
