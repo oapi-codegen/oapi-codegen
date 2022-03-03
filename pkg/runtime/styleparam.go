92b48099b371c75f68d0ddd548ae38c929a96485
Stephen DeSanto <anoryx@anoryx.com>
2022-03-03 10:59:29 -0800

    Parse int16 parameters in client code
    All of the other int types are covered by `primitiveToString`; this
    completes the set.

--- pkg/runtime/styleparam.go
+++ pkg/runtime/styleparam.go
@@ -354,7 +354,7 @@ func primitiveToString(value interface{}) (string, error) {
 	kind := t.Kind()
 
 	switch kind {
-	case reflect.Int8, reflect.Int32, reflect.Int64, reflect.Int:
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
 		output = strconv.FormatInt(v.Int(), 10)
 	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint:
 		output = strconv.FormatUint(v.Uint(), 10)
