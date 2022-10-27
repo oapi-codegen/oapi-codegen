7b010099dcf1192b3bfaa3898b5f375bb9590ddf
Jedri Visser <git@jedrivisser.com>
2022-10-27 20:18:06 +0200

    Add client-type-prefix output option (#785) (#788)
    * Add client-type-prefix output option (#785)
    
    * Override whole client type instead of prefixing it

--- pkg/codegen/template_helpers.go
+++ pkg/codegen/template_helpers.go
@@ -27,6 +27,8 @@ import (
 const (
 	// These allow the case statements to be sorted later:
 	prefixMostSpecific, prefixLessSpecific, prefixLeastSpecific = "3", "6", "9"
+
+	defaultClientTypeName = "Client"
 )
 
 var (
