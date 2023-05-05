8fbbe02254ebc3a9359a5565859770aaf2122e41
Christoph Petrausch <263448+hikhvar@users.noreply.github.com>
2023-05-05 23:52:29 +0200

    fix: Use helper function to detect general JSON responses (#945)
    With out this fix, there are no responses generated if for example the response type is 'application/vnd.api+json'
    
    Co-authored-by: Marcin Romaszewicz <47459980+deepmap-marcinr@users.noreply.github.com>

--- pkg/codegen/operations.go
+++ pkg/codegen/operations.go
@@ -297,7 +297,7 @@ func (o *OperationDefinition) GetResponseTypeDefinitions() ([]ResponseTypeDefini
 					case StringInArray(contentTypeName, contentTypesHalJSON):
 						typeName = fmt.Sprintf("HALJSON%s", ToCamelCase(responseName))
 					// JSON:
-					case StringInArray(contentTypeName, contentTypesJSON):
+					case StringInArray(contentTypeName, contentTypesJSON) || util.IsMediaTypeJson(contentTypeName):
 						typeName = fmt.Sprintf("JSON%s", ToCamelCase(responseName))
 					// YAML:
 					case StringInArray(contentTypeName, contentTypesYAML):
