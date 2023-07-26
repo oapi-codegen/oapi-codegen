d9db08decbe1ba1272b66c426ac8922d26b50be2
gmtstephane <gmtstephane@gmail.com>
2023-07-26 12:09:08 +0200

    fix warn log condition

--- pkg/chi-middleware/oapi_validate.go
+++ pkg/chi-middleware/oapi_validate.go
@@ -40,7 +40,7 @@ func OapiRequestValidator(swagger *openapi3.T) func(next http.Handler) http.Hand
 // OapiRequestValidatorWithOptions Creates middleware to validate request by swagger spec.
 // This middleware is good for net/http either since go-chi is 100% compatible with net/http.
 func OapiRequestValidatorWithOptions(swagger *openapi3.T, options *Options) func(next http.Handler) http.Handler {
-	if swagger.Servers != nil && (options == nil || options.SilenceServersWarning) {
+	if swagger.Servers != nil && (options == nil || !options.SilenceServersWarning) {
 		log.Println("WARN: OapiRequestValidatorWithOptions called with an OpenAPI spec that has `Servers` set. This may lead to an HTTP 400 with `no matching operation was found` when sending a valid request, as the validator performs `Host` header validation. If you're expecting `Host` header validation, you can silence this warning by setting `Options.SilenceServersWarning = true`. See https://github.com/deepmap/oapi-codegen/issues/882 for more information.")
 	}
 
