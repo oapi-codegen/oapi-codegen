ed8f5debebea44b072029e441d66b43fd84643ef
Jamie Tanna <jamie@jamietanna.co.uk>
2023-08-24 13:23:52 +0100

    sq

--- pkg/codegen/template_helpers.go
+++ pkg/codegen/template_helpers.go
@@ -243,7 +243,6 @@ func genResponseTypeName(operationID string) string {
 }
 
 func getResponseTypeDefinitions(op *OperationDefinition) []ResponseTypeDefinition {
-	fmt.Printf("op: %v\n", op)
 	td, err := op.GetResponseTypeDefinitions()
 	if err != nil {
 		panic(err)
