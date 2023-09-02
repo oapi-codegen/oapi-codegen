3094d0b3a091e0a82773d16b46329172c5cace8b
Jamie Tanna <jamie@jamietanna.co.uk>
2023-09-02 16:37:02 +0100

    Remove dependency on moq
    We're not really actually using moq for anything heavyweight, so we can
    replace the usage with a fake server.
    
    Closes #1230.

--- internal/test/server/server_test.go
+++ internal/test/server/server_test.go
@@ -10,12 +10,86 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParameters(t *testing.T) {
-	m := ServerInterfaceMock{}
+type fakeServer struct {
+	t      *testing.T
+	called bool
+}
 
-	m.CreateResource2Func = func(w http.ResponseWriter, r *http.Request, inlineArgument int, params CreateResource2Params) {
-		assert.Equal(t, 99, *params.InlineQueryArgument)
-		assert.Equal(t, 1, inlineArgument)
+// get every type optional
+// (GET /every-type-optional)
+func (s *fakeServer) GetEveryTypeOptional(w http.ResponseWriter, r *http.Request) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// Get resource via simple path
+// (GET /get-simple)
+func (s *fakeServer) GetSimple(w http.ResponseWriter, r *http.Request) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// Getter with referenced parameter and referenced response
+// (GET /get-with-args)
+func (s *fakeServer) GetWithArgs(w http.ResponseWriter, r *http.Request, params GetWithArgsParams) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// Getter with referenced parameter and referenced response
+// (GET /get-with-references/{global_argument}/{argument})
+func (s *fakeServer) GetWithReferences(w http.ResponseWriter, r *http.Request, globalArgument int64, argument Argument) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// Get an object by ID
+// (GET /get-with-type/{content_type})
+func (s *fakeServer) GetWithContentType(w http.ResponseWriter, r *http.Request, contentType GetWithContentTypeParamsContentType) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// get with reserved keyword
+// (GET /reserved-keyword)
+func (s *fakeServer) GetReservedKeyword(w http.ResponseWriter, r *http.Request) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// Create a resource
+// (POST /resource/{argument})
+func (s *fakeServer) CreateResource(w http.ResponseWriter, r *http.Request, argument Argument) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// Create a resource with inline parameter
+// (POST /resource2/{inline_argument})
+func (s *fakeServer) CreateResource2(w http.ResponseWriter, r *http.Request, inlineArgument int, params CreateResource2Params) {
+	assert.Equal(s.t, 99, *params.InlineQueryArgument)
+	assert.Equal(s.t, 1, inlineArgument)
+	s.called = true
+}
+
+// Update a resource with inline body. The parameter name is a reserved
+// keyword, so make sure that gets prefixed to avoid syntax errors
+// (PUT /resource3/{fallthrough})
+func (s *fakeServer) UpdateResource3(w http.ResponseWriter, r *http.Request, pFallthrough int) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+// get response with reference
+// (GET /response-with-reference)
+func (s *fakeServer) GetResponseWithReference(w http.ResponseWriter, r *http.Request) {
+	// not implemented
+	w.WriteHeader(http.StatusTeapot)
+}
+
+func TestParameters(t *testing.T) {
+	m := fakeServer{
+		t: t,
 	}
 
 	h := Handler(&m)
@@ -24,11 +98,11 @@ func TestParameters(t *testing.T) {
 	rr := httptest.NewRecorder()
 	h.ServeHTTP(rr, req)
 
-	assert.Equal(t, 1, len(m.CreateResource2Calls()))
+	assert.True(t, m.called)
 }
 
 func TestErrorHandlerFunc(t *testing.T) {
-	m := ServerInterfaceMock{}
+	m := fakeServer{}
 
 	h := HandlerWithOptions(&m, ChiServerOptions{
 		ErrorHandlerFunc: func(w http.ResponseWriter, r *http.Request, err error) {
@@ -47,7 +121,7 @@ func TestErrorHandlerFunc(t *testing.T) {
 }
 
 func TestErrorHandlerFuncBackwardsCompatible(t *testing.T) {
-	m := ServerInterfaceMock{}
+	m := fakeServer{}
 
 	h := HandlerWithOptions(&m, ChiServerOptions{})
 
