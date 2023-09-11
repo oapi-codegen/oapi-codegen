19373ab96086a7d8c6cec20059c918fb2e42db54
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-09-11 11:56:18 +0900

    Test code for issue #1250

---
+++ internal/test/issues/issue-1250/issue_test.go
@@ -0,0 +1,37 @@
+package issue1250_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	issue1250 "github.com/deepmap/oapi-codegen/internal/test/issues/issue-1250"
+	"github.com/stretchr/testify/assert"
+)
+
+type testStrictServerInterface struct {
+	t *testing.T
+}
+
+func (s *testStrictServerInterface) Test(ctx context.Context, request issue1250.TestRequestObject) (issue1250.TestResponseObject, error) {
+	s1 := "foo"
+	r := issue1250.Test{
+		Field1: &s1,
+	}
+	r.Set("Extra", "bar")
+	return issue1250.Test200JSONResponse(r), nil
+}
+
+func TestIssue1250(t *testing.T) {
+	si := issue1250.NewStrictHandler(&testStrictServerInterface{
+		t: t,
+	}, nil)
+
+	req := httptest.NewRequest("GET", "http://localhost/test", nil)
+	rec := httptest.NewRecorder()
+	si.Test(rec, req)
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.JSONEq(t, `{"field1": "foo", "Extra": "bar"}`, rec.Body.String())
+}
