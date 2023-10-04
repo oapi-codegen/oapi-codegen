d2d949e0115558b2c0957f75a91cd561a0daa35d
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-10-04 11:51:53 +0900

    Test code for #1302

---
+++ internal/test/issues/issue-1302/doc.go
@@ -0,0 +1,3 @@
+package issue1302
+
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml issue1302.yaml
---
+++ internal/test/issues/issue-1302/issue1302_test.go
@@ -0,0 +1,15 @@
+package issue1302_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	issue1302 "github.com/deepmap/oapi-codegen/v2/internal/test/issues/issue-1302"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIssue1302(t *testing.T) {
+	buf, err := json.Marshal(issue1302.Test{})
+	assert.NoError(t, err)
+	assert.JSONEq(t, `{"Object":{}}`, string(buf))
+}
