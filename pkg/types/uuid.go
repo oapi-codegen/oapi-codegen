377d7e72257446d62e6a7092d3430864598af5b0
Jamie Tanna <jamie@jamietanna.co.uk>
2023-10-10 09:24:05 +0100

    Remove deprecated packages
    As part of #1308 and #1142, ahead of the v2 release, we want to remove
    any deprecated packages to reduce the dependency tree, and nudge folks
    to use the separated packages.
    
    Closes #1308, #1142.

--- pkg/types/uuid.go
+++
@@ -1,8 +0,0 @@
-package types
-
-import (
-	"github.com/google/uuid"
-)
-
-// Deprecated: This has been replaced by https://pkg.go.dev/github.com/oapi-codegen/runtime/types#UUID
-type UUID = uuid.UUID
