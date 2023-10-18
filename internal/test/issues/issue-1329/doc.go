1a08d2ed970e48667382d70dde02d62dbd2f82a7
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-10-18 15:11:00 +0900

    Test code for #1329

---
+++ internal/test/issues/issue-1329/doc.go
@@ -0,0 +1,3 @@
+package issue1329
+
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.yaml issue1329.yaml
---
+++ internal/test/issues/issue-1329/issue1329_test.go
@@ -0,0 +1,31 @@
+package issue1329_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	issue1329 "github.com/deepmap/oapi-codegen/internal/test/issues/issue-1329"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIssue1329(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/bad+json")
+		w.WriteHeader(204)
+	}))
+	defer ts.Close()
+
+	c, err := issue1329.NewClientWithResponses(ts.URL)
+	assert.NoError(t, err)
+	res, err := c.TestWithApplicationTestPlusJSONBodyWithResponse(
+		context.TODO(),
+		issue1329.Test{
+			Field1: "test1",
+			Field2: "test2",
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, 204, res.StatusCode())
+}
