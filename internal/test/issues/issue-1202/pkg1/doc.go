307af562e3fb85f457cb29a43868d630bacec7a6
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-12-06 14:11:06 +0900

    Fix module path of test codes

--- internal/test/issues/issue-1202/pkg1/doc.go
+++ internal/test/issues/issue-1202/pkg1/doc.go
@@ -1,3 +1,3 @@
 package pkg1
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.yaml ../pkg1.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml ../pkg1.yaml
