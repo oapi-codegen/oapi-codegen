e2ade0308d30e0b475a0964f405ea7c1982d0a4a
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2023-12-07 09:24:09 +0900

    Add test for issue-1373

---
+++ internal/test/issues/issue-1373/issue_test.go
@@ -0,0 +1,30 @@
+package issue1373
+
+import (
+	_ "embed"
+
+	"testing"
+
+	"github.com/deepmap/oapi-codegen/v2/pkg/codegen"
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/stretchr/testify/require"
+)
+
+//go:embed spec.yaml
+var spec []byte
+
+// Test treatment additionalProperties in mergeOpenapiSchemas()
+func TestIssue(t *testing.T) {
+	swagger, err := openapi3.NewLoader().LoadFromData(spec)
+	require.NoError(t, err)
+
+	opts := codegen.Configuration{
+		PackageName: "issue1373",
+		Generate: codegen.GenerateOptions{
+			Models: true,
+		},
+	}
+
+	_, err = codegen.Generate(swagger, opts)
+	require.NoError(t, err)
+}
