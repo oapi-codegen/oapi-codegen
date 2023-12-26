70daff287d07076ba0d7cc4cf17b77714327e0cf
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-12-26 14:29:40 +0900

    Refactor code generator

--- internal/test/strict-server-response/generator/generate.go
+++ internal/test/strict-server-response/generator/generate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -39,30 +40,8 @@ type strictServerInterface struct{}
 
 	for _, ref := range []bool{false, true} {
 		for _, extRef := range []bool{false, true} {
-			nameSlice := []string{"test"}
-			var pkgId int = 1
-			if ref {
-				if extRef {
-					nameSlice = append(nameSlice, "Ext")
-					pkgId = 2
-				} else {
-					nameSlice = append(nameSlice, "Ref")
-				}
-			} else {
-				if extRef {
-					continue
-				}
-			}
 			for _, header := range []bool{false, true} {
-				nameSlice := nameSlice
-				if header {
-					nameSlice = append(nameSlice, "Header")
-				}
 				for _, fixedStatusCode := range []bool{false, true} {
-					nameSlice := nameSlice
-					if fixedStatusCode {
-						nameSlice = append(nameSlice, "Fixed")
-					}
 					for _, content := range []struct {
 						name    []string
 						content string
@@ -95,251 +74,7 @@ type strictServerInterface struct{}
 							continue
 						}
 
-						nameSlice := append(nameSlice, content.name...)
-
-						response := map[string]any{}
-
-						if content.content != "" {
-							response["content"] = map[string]any{
-								content.content: map[string]any{
-									"schema": map[string]any{
-										"$ref": "#/components/schemas/TestSchema",
-									},
-								},
-							}
-						}
-
-						if header {
-							response["headers"] = map[string]any{
-								"header1": map[string]any{
-									"schema": map[string]any{
-										"type": "string",
-									},
-								},
-								"header2": map[string]any{
-									"schema": map[string]any{
-										"type": "integer",
-									},
-								},
-							}
-						}
-
-						var statusCode string
-						if fixedStatusCode {
-							if content.content == "" {
-								statusCode = "204"
-							} else {
-								statusCode = "200"
-							}
-						} else {
-							statusCode = "default"
-						}
-
-						var responseInResponses map[string]any
-						if ref {
-							responseName := "testResp" + strings.Join(nameSlice[1:], "")
-							responses[pkgId][responseName] = response
-							var ref string
-							if pkgId == 1 {
-								ref = fmt.Sprintf("#/components/responses/%s", responseName)
-							} else {
-								ref = fmt.Sprintf("pkg%d.yaml#/components/responses/%s", pkgId, responseName)
-							}
-							responseInResponses = map[string]any{
-								"$ref": ref,
-							}
-						} else {
-							responseInResponses = response
-						}
-
-						pathSlice := make([]string, 0, len(nameSlice))
-						for _, s := range nameSlice {
-							pathSlice = append(pathSlice, strings.ToLower(s))
-						}
-						path := "/" + strings.Join(pathSlice, "-")
-						paths[path] = map[string]any{
-							"get": map[string]any{
-								"operationId": strings.Join(nameSlice, ""),
-								"responses": map[string]any{
-									statusCode: responseInResponses,
-								},
-							},
-						}
-
-						method := "Test" + strings.Join(nameSlice[1:], "")
-						body := ""
-						var statusCodeRes int
-						if content.content == "" {
-							statusCodeRes = 204
-						} else {
-							statusCodeRes = 200
-						}
-						switch {
-						case content.content == "":
-						case content.content == "text/plain":
-							body = "\"bar\""
-						case strings.HasPrefix(content.content, "multipart/"):
-							body = fmt.Sprintf(`func(writer *multipart.Writer) error {
-								if p, err := writer.CreatePart(textproto.MIMEHeader{"Content-Type": []string{"application/json"}}); err != nil {
-									return err
-								} else {
-									return json.NewEncoder(p).Encode(pkg%d.TestSchema{
-										Field1: "bar",
-										Field2: 456,
-									})
-								}
-					}`, pkgId)
-						case content.content == "application/test" || content.content == "application/*":
-							body = "bytes.NewReader(buf)"
-						default:
-							body = fmt.Sprintf(`pkg%d.TestSchema{
-								Field1: "bar",
-								Field2: 456,
-					}`, pkgId)
-						}
-						contentRes := content.content
-						if strings.HasSuffix(contentRes, "/*") {
-							contentRes = strings.TrimSuffix(contentRes, "/*") + "/baz"
-						}
-						fmt.Fprintf(fTestGo, "func (s strictServerInterface) %s(ctx context.Context, request pkg1.%sRequestObject) (pkg1.%sResponseObject, error) {\n", method, method, method)
-						switch content.content {
-						case "application/test", "application/*":
-							fmt.Fprintf(fTestGo, "buf := []byte(\"bar\")\n")
-						}
-						var resRet string
-						if ref && fixedStatusCode {
-							resRet = fmt.Sprintf("pkg%d.TestResp%s%sResponse", pkgId, strings.Join(nameSlice[1:], ""), content.tag)
-						} else {
-							resRet = fmt.Sprintf("pkg1.%s%s%sResponse", method, statusCode, content.tag)
-						}
-
-						if header || !fixedStatusCode ||
-							content.content == "application/test" || content.content == "application/*" ||
-							content.content == "multipart/*" || content.content == "" {
-							resRet += " {\n"
-							if body != "" {
-								resRet += fmt.Sprintf("Body: %s,\n", body)
-							}
-							if header {
-								var headerType string
-								if ref {
-									headerType = fmt.Sprintf("pkg%d.TestResp%sResponseHeaders", pkgId, strings.Join(nameSlice[1:], ""))
-								} else {
-									headerType = fmt.Sprintf("pkg1.%s%sResponseHeaders", method, statusCode)
-								}
-								resRet += fmt.Sprintf(`Headers: %s {
-									Header1: "foo",
-									Header2: 123,
-								},
-								`, headerType)
-							}
-							if !fixedStatusCode {
-								resRet += fmt.Sprintf("StatusCode: %d,\n", statusCodeRes)
-							}
-							if strings.HasSuffix(content.content, "/*") {
-								resRet += fmt.Sprintf("ContentType: \"%s\",\n", contentRes)
-							}
-							switch content.content {
-							case "application/test", "application/*":
-								resRet += "ContentLength: int64(len(buf)),\n"
-							}
-							resRet += "}"
-						} else {
-							resRet += "(" + body + ")"
-						}
-						if ref && fixedStatusCode {
-							if (strings.HasPrefix(content.content, "multipart/") && !header) ||
-								(content.content == "" && extRef) {
-								resRet = fmt.Sprintf("pkg1.%s%s%sResponse(%s)", method, statusCode, content.tag, resRet)
-							} else if content.content != "" {
-								resRet = fmt.Sprintf("pkg1.%s%s%sResponse{%s}", method, statusCode, content.tag, resRet)
-							}
-						}
-						fmt.Fprintf(fTestGo, "return %s, nil\n", resRet)
-						fmt.Fprintf(fTestGo, "}\n")
-						fmt.Fprintf(fTestGo, "\n")
-
-						fmt.Fprintf(fTestGo, `func %s(t *testing.T) {
-							hh := pkg1.Handler(pkg1.NewStrictHandler(strictServerInterface{}, nil))
-
-							ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-								if !assert.Equal(t, "%s", r.URL.Path) {
-									w.WriteHeader(http.StatusInternalServerError)
-									return
-								}
-								hh.ServeHTTP(w, r)
-							}))
-							defer ts.Close()
-
-							c, err := pkg1.NewClientWithResponses(ts.URL)
-							assert.NoError(t, err)
-							res, err := c.%sWithResponse(context.TODO())
-							assert.NoError(t, err)
-							assert.Equal(t, %d, res.StatusCode())
-						`, method, path, method, statusCodeRes)
-						if header {
-							fmt.Fprintf(fTestGo, "assert.Equal(t, \"foo\", res.HTTPResponse.Header.Get(\"header1\"))\n")
-							fmt.Fprintf(fTestGo, "assert.Equal(t, \"123\", res.HTTPResponse.Header.Get(\"header2\"))\n")
-						}
-						if !strings.HasPrefix(contentRes, "multipart/") {
-							fmt.Fprintf(fTestGo, "assert.Equal(t, \"%s\", res.HTTPResponse.Header.Get(\"Content-Type\"))\n", contentRes)
-						}
-						switch {
-						case content.content == "":
-							fmt.Fprintf(fTestGo, "assert.Equal(t, []byte{}, res.Body)\n")
-						case strings.HasPrefix(content.content, "multipart/"):
-							fmt.Fprintf(fTestGo, `mediaType, params, err := mime.ParseMediaType(res.HTTPResponse.Header.Get("Content-Type"))
-								if assert.NoError(t, err) {
-									assert.Equal(t, "%s", mediaType)
-									assert.NotEmpty(t, params["boundary"])
-									reader := multipart.NewReader(bytes.NewReader(res.Body), params["boundary"])
-									jsonExist := false
-									for {
-										if p, err := reader.NextPart(); err == io.EOF {
-											break
-										} else {
-											assert.NoError(t, err)
-											switch p.Header.Get("Content-Type") {
-											case "application/json":
-												var j pkg%d.TestSchema
-												err := json.NewDecoder(p).Decode(&j)
-												assert.NoError(t, err)
-												assert.Equal(t, pkg%d.TestSchema{
-													Field1: "bar",
-													Field2: 456,
-												}, j)
-												jsonExist = true
-											default:
-												assert.Fail(t, "Bad Content-Type: %%s", p.Header.Get("Content-Type"))
-											}
-										}
-									}
-									assert.True(t, jsonExist)
-								}
-					`, contentRes, pkgId, pkgId)
-						case content.content == "application/x-www-form-urlencoded":
-							fmt.Fprintf(fTestGo, `form, err := url.ParseQuery(string(res.Body))
-								assert.NoError(t, err)
-								assert.Equal(t, url.Values{
-									"field1": []string{"bar"},
-									"field2": []string{"456"},
-								}, form)
-					`)
-						case content.content == "application/json":
-							fmt.Fprintf(fTestGo, "assert.Equal(t, &pkg%d.TestSchema{\n", pkgId)
-							fmt.Fprintf(fTestGo, "Field1: \"bar\",\n")
-							fmt.Fprintf(fTestGo, "Field2: 456,\n")
-							fmt.Fprintf(fTestGo, "}, res.JSON%s%s)\n", strings.ToUpper(statusCode[:1]), statusCode[1:])
-						case content.content == "application/test+json":
-							fmt.Fprintf(fTestGo, "assert.Equal(t, &pkg%d.TestSchema{\n", pkgId)
-							fmt.Fprintf(fTestGo, "Field1: \"bar\",\n")
-							fmt.Fprintf(fTestGo, "Field2: 456,\n")
-							fmt.Fprintf(fTestGo, "}, res.ApplicationtestJSON%s%s)\n", strings.ToUpper(statusCode[:1]), statusCode[1:])
-						default:
-							fmt.Fprintf(fTestGo, "assert.Equal(t, []byte(\"bar\"), res.Body)\n")
-						}
-						fmt.Fprintf(fTestGo, "}\n")
-						fmt.Fprintf(fTestGo, "\n")
+						generateOneTest(fTestGo, paths, responses, ref, extRef, header, fixedStatusCode, content)
 					}
 				}
 			}
@@ -429,4 +164,277 @@ type strictServerInterface struct{}
 			panic(err)
 		}
 	}
+}
+
+func generateOneTest(fTestGo io.Writer, paths map[string]any, responses map[int]map[string]any,
+	ref bool, extRef bool, header bool, fixedStatusCode bool, content struct {
+		name    []string
+		content string
+		tag     string
+	}) {
+	nameSlice := []string{"test"}
+	var pkgId int = 1
+	if ref {
+		if extRef {
+			nameSlice = append(nameSlice, "Ext")
+			pkgId = 2
+		} else {
+			nameSlice = append(nameSlice, "Ref")
+		}
+	} else {
+		if extRef {
+			return
+		}
+	}
+	if header {
+		nameSlice = append(nameSlice, "Header")
+	}
+	if fixedStatusCode {
+		nameSlice = append(nameSlice, "Fixed")
+	}
+	nameSlice = append(nameSlice, content.name...)
+
+	response := map[string]any{}
+
+	if content.content != "" {
+		response["content"] = map[string]any{
+			content.content: map[string]any{
+				"schema": map[string]any{
+					"$ref": "#/components/schemas/TestSchema",
+				},
+			},
+		}
+	}
+
+	if header {
+		response["headers"] = map[string]any{
+			"header1": map[string]any{
+				"schema": map[string]any{
+					"type": "string",
+				},
+			},
+			"header2": map[string]any{
+				"schema": map[string]any{
+					"type": "integer",
+				},
+			},
+		}
+	}
+
+	var statusCode string
+	if fixedStatusCode {
+		if content.content == "" {
+			statusCode = "204"
+		} else {
+			statusCode = "200"
+		}
+	} else {
+		statusCode = "default"
+	}
+
+	var responseInResponses map[string]any
+	if ref {
+		responseName := "testResp" + strings.Join(nameSlice[1:], "")
+		responses[pkgId][responseName] = response
+		var ref string
+		if pkgId == 1 {
+			ref = fmt.Sprintf("#/components/responses/%s", responseName)
+		} else {
+			ref = fmt.Sprintf("pkg%d.yaml#/components/responses/%s", pkgId, responseName)
+		}
+		responseInResponses = map[string]any{
+			"$ref": ref,
+		}
+	} else {
+		responseInResponses = response
+	}
+
+	pathSlice := make([]string, 0, len(nameSlice))
+	for _, s := range nameSlice {
+		pathSlice = append(pathSlice, strings.ToLower(s))
+	}
+	path := "/" + strings.Join(pathSlice, "-")
+	paths[path] = map[string]any{
+		"get": map[string]any{
+			"operationId": strings.Join(nameSlice, ""),
+			"responses": map[string]any{
+				statusCode: responseInResponses,
+			},
+		},
+	}
+
+	method := "Test" + strings.Join(nameSlice[1:], "")
+	body := ""
+	var statusCodeRes int
+	if content.content == "" {
+		statusCodeRes = 204
+	} else {
+		statusCodeRes = 200
+	}
+	switch {
+	case content.content == "":
+	case content.content == "text/plain":
+		body = "\"bar\""
+	case strings.HasPrefix(content.content, "multipart/"):
+		body = fmt.Sprintf(`func(writer *multipart.Writer) error {
+			if p, err := writer.CreatePart(textproto.MIMEHeader{"Content-Type": []string{"application/json"}}); err != nil {
+				return err
+			} else {
+				return json.NewEncoder(p).Encode(pkg%d.TestSchema{
+					Field1: "bar",
+					Field2: 456,
+				})
+			}
+}`, pkgId)
+	case content.content == "application/test" || content.content == "application/*":
+		body = "bytes.NewReader(buf)"
+	default:
+		body = fmt.Sprintf(`pkg%d.TestSchema{
+			Field1: "bar",
+			Field2: 456,
+}`, pkgId)
+	}
+	contentRes := content.content
+	if strings.HasSuffix(contentRes, "/*") {
+		contentRes = strings.TrimSuffix(contentRes, "/*") + "/baz"
+	}
+	fmt.Fprintf(fTestGo, "func (s strictServerInterface) %s(ctx context.Context, request pkg1.%sRequestObject) (pkg1.%sResponseObject, error) {\n", method, method, method)
+	switch content.content {
+	case "application/test", "application/*":
+		fmt.Fprintf(fTestGo, "buf := []byte(\"bar\")\n")
+	}
+	var resRet string
+	if ref && fixedStatusCode {
+		resRet = fmt.Sprintf("pkg%d.TestResp%s%sResponse", pkgId, strings.Join(nameSlice[1:], ""), content.tag)
+	} else {
+		resRet = fmt.Sprintf("pkg1.%s%s%sResponse", method, statusCode, content.tag)
+	}
+
+	if header || !fixedStatusCode ||
+		content.content == "application/test" || content.content == "application/*" ||
+		content.content == "multipart/*" || content.content == "" {
+		resRet += " {\n"
+		if body != "" {
+			resRet += fmt.Sprintf("Body: %s,\n", body)
+		}
+		if header {
+			var headerType string
+			if ref {
+				headerType = fmt.Sprintf("pkg%d.TestResp%sResponseHeaders", pkgId, strings.Join(nameSlice[1:], ""))
+			} else {
+				headerType = fmt.Sprintf("pkg1.%s%sResponseHeaders", method, statusCode)
+			}
+			resRet += fmt.Sprintf(`Headers: %s {
+				Header1: "foo",
+				Header2: 123,
+			},
+			`, headerType)
+		}
+		if !fixedStatusCode {
+			resRet += fmt.Sprintf("StatusCode: %d,\n", statusCodeRes)
+		}
+		if strings.HasSuffix(content.content, "/*") {
+			resRet += fmt.Sprintf("ContentType: \"%s\",\n", contentRes)
+		}
+		switch content.content {
+		case "application/test", "application/*":
+			resRet += "ContentLength: int64(len(buf)),\n"
+		}
+		resRet += "}"
+	} else {
+		resRet += "(" + body + ")"
+	}
+	if ref && fixedStatusCode {
+		if (strings.HasPrefix(content.content, "multipart/") && !header) ||
+			(content.content == "" && extRef) {
+			resRet = fmt.Sprintf("pkg1.%s%s%sResponse(%s)", method, statusCode, content.tag, resRet)
+		} else if content.content != "" {
+			resRet = fmt.Sprintf("pkg1.%s%s%sResponse{%s}", method, statusCode, content.tag, resRet)
+		}
+	}
+	fmt.Fprintf(fTestGo, "return %s, nil\n", resRet)
+	fmt.Fprintf(fTestGo, "}\n")
+	fmt.Fprintf(fTestGo, "\n")
+
+	fmt.Fprintf(fTestGo, `func %s(t *testing.T) {
+		hh := pkg1.Handler(pkg1.NewStrictHandler(strictServerInterface{}, nil))
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !assert.Equal(t, "%s", r.URL.Path) {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			hh.ServeHTTP(w, r)
+		}))
+		defer ts.Close()
+
+		c, err := pkg1.NewClientWithResponses(ts.URL)
+		assert.NoError(t, err)
+		res, err := c.%sWithResponse(context.TODO())
+		assert.NoError(t, err)
+		assert.Equal(t, %d, res.StatusCode())
+	`, method, path, method, statusCodeRes)
+	if header {
+		fmt.Fprintf(fTestGo, "assert.Equal(t, \"foo\", res.HTTPResponse.Header.Get(\"header1\"))\n")
+		fmt.Fprintf(fTestGo, "assert.Equal(t, \"123\", res.HTTPResponse.Header.Get(\"header2\"))\n")
+	}
+	if !strings.HasPrefix(contentRes, "multipart/") {
+		fmt.Fprintf(fTestGo, "assert.Equal(t, \"%s\", res.HTTPResponse.Header.Get(\"Content-Type\"))\n", contentRes)
+	}
+	switch {
+	case content.content == "":
+		fmt.Fprintf(fTestGo, "assert.Equal(t, []byte{}, res.Body)\n")
+	case strings.HasPrefix(content.content, "multipart/"):
+		fmt.Fprintf(fTestGo, `mediaType, params, err := mime.ParseMediaType(res.HTTPResponse.Header.Get("Content-Type"))
+			if assert.NoError(t, err) {
+				assert.Equal(t, "%s", mediaType)
+				assert.NotEmpty(t, params["boundary"])
+				reader := multipart.NewReader(bytes.NewReader(res.Body), params["boundary"])
+				jsonExist := false
+				for {
+					if p, err := reader.NextPart(); err == io.EOF {
+						break
+					} else {
+						assert.NoError(t, err)
+						switch p.Header.Get("Content-Type") {
+						case "application/json":
+							var j pkg%d.TestSchema
+							err := json.NewDecoder(p).Decode(&j)
+							assert.NoError(t, err)
+							assert.Equal(t, pkg%d.TestSchema{
+								Field1: "bar",
+								Field2: 456,
+							}, j)
+							jsonExist = true
+						default:
+							assert.Fail(t, "Bad Content-Type: %%s", p.Header.Get("Content-Type"))
+						}
+					}
+				}
+				assert.True(t, jsonExist)
+			}
+`, contentRes, pkgId, pkgId)
+	case content.content == "application/x-www-form-urlencoded":
+		fmt.Fprintf(fTestGo, `form, err := url.ParseQuery(string(res.Body))
+			assert.NoError(t, err)
+			assert.Equal(t, url.Values{
+				"field1": []string{"bar"},
+				"field2": []string{"456"},
+			}, form)
+`)
+	case content.content == "application/json":
+		fmt.Fprintf(fTestGo, "assert.Equal(t, &pkg%d.TestSchema{\n", pkgId)
+		fmt.Fprintf(fTestGo, "Field1: \"bar\",\n")
+		fmt.Fprintf(fTestGo, "Field2: 456,\n")
+		fmt.Fprintf(fTestGo, "}, res.JSON%s%s)\n", strings.ToUpper(statusCode[:1]), statusCode[1:])
+	case content.content == "application/test+json":
+		fmt.Fprintf(fTestGo, "assert.Equal(t, &pkg%d.TestSchema{\n", pkgId)
+		fmt.Fprintf(fTestGo, "Field1: \"bar\",\n")
+		fmt.Fprintf(fTestGo, "Field2: 456,\n")
+		fmt.Fprintf(fTestGo, "}, res.ApplicationtestJSON%s%s)\n", strings.ToUpper(statusCode[:1]), statusCode[1:])
+	default:
+		fmt.Fprintf(fTestGo, "assert.Equal(t, []byte(\"bar\"), res.Body)\n")
+	}
+	fmt.Fprintf(fTestGo, "}\n")
+	fmt.Fprintf(fTestGo, "\n")
 }
