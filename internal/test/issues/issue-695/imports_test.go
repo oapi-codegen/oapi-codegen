e7a200108b5b0c6c563a5c790c4c733353b473b3
Jamie Tanna <jamie@jamietanna.co.uk>
2024-01-19 10:19:59 +0000

    rew! Add test code from 695
    As raised as part of #695,

---
+++ internal/test/issues/issue-695/imports_test.go
@@ -0,0 +1,29 @@
+package externalref
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	packageA "github.com/deepmap/oapi-codegen/v2/internal/test/issues/issue-695/packageA"
+	packageB "github.com/deepmap/oapi-codegen/v2/internal/test/issues/issue-695/packageB"
+)
+
+func TestParameters(t *testing.T) {
+	b := &packageB.ObjectB{}
+	_ = Container{
+		ObjectA: &packageA.ObjectA{ObjectB: b},
+		ObjectB: b,
+	}
+}
+
+func TestGetSwagger(t *testing.T) {
+	_, err := packageB.GetSwagger()
+	require.Nil(t, err)
+
+	_, err = packageA.GetSwagger()
+	require.Nil(t, err)
+
+	_, err = GetSwagger()
+	require.Nil(t, err)
+}
