5dfbf8ff683b9249b1fbf5aa6b8cb169d31e3fdf
soeenplop <82885605+soeenplop@users.noreply.github.com>
2024-07-15 18:48:34 +0200

    Add fix for recursive generation of named response types

--- internal/test/issues/issue-1306/issue_test.go
+++ internal/test/issues/issue-1306/issue_test.go
@@ -1,16 +1,25 @@
 package issue1306_test
 
 import (
-	issue1306 "github.com/deepmap/oapi-codegen/internal/test/issues/issue-1306"
+	issue1306 "github.com/oapi-codegen/oapi-codegen/v2/internal/test/issues/issue-1306"
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
 
 func TestIssue1306(t *testing.T) {
-	actualType := reflect.TypeOf(issue1306.SomeObject{})
+	actualType := reflect.TypeOf(issue1306.SomeType{})
 	expectedType := reflect.TypeOf(struct {
-		Name *string `json:"name,omitempty"`
+		Name    *string                    `json:"name,omitempty"`
+		Objects *[]issue1306.SomeChildType `json:"objects,omitempty"`
+	}{})
+
+	assert.True(t, actualType.AssignableTo(expectedType))
+
+	actualType = reflect.TypeOf(issue1306.SomeChildType{})
+	expectedType = reflect.TypeOf(struct {
+		Key   *string `json:"key,omitempty"`
+		Value *string `json:"value,omitempty"`
 	}{})
 
 	assert.True(t, actualType.AssignableTo(expectedType))
