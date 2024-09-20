5bfe59a861f54ca81f1e3c6b5d6f2c64840dc5a1
Jamie Tanna <jamie.tanna@elastic.co>
2024-09-20 17:08:48 +0100

    sq

---
+++ examples/import-mapping/multiplefiles/generate.go
@@ -0,0 +1,4 @@
+package multiplefiles
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg-api.yaml api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg-user.yaml user.yaml
