22c8c074099bc795d62d14bacf228e2c677c0d81
cosban <me@cosban.net>
2024-10-28 12:31:13 -0600

    add literal colon support for gin and echo

--- pkg/codegen/utils.go
+++ pkg/codegen/utils.go
@@ -543,6 +543,7 @@ func SwaggerUriToIrisUri(uri string) string {
 //	{?param}
 //	{?param*}
 func SwaggerUriToEchoUri(uri string) string {
+	uri = strings.ReplaceAll(uri, ":", "\\:")
 	return pathParamRE.ReplaceAllString(uri, ":$1")
 }
 
@@ -591,6 +592,7 @@ func SwaggerUriToChiUri(uri string) string {
 //	{?param}
 //	{?param*}
 func SwaggerUriToGinUri(uri string) string {
+	uri = strings.ReplaceAll(uri, ":", "\\:")
 	return pathParamRE.ReplaceAllString(uri, ":$1")
 }
 
