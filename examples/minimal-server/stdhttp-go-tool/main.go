2647a108f5fd2efaa11e398f596361331830bc5a
Jamie Tanna <jamie.tanna@elastic.co>
2025-02-20 15:30:15 +0000

    docs: add example of using `go tool` for Go 1.24+

---
+++ examples/minimal-server/stdhttp-go-tool/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/oapi-codegen/oapi-codegen/v2/examples/minimal-server/stdhttp-go-tool/api"
+)
+
+func main() {
+	// create a type that satisfies the `api.ServerInterface`, which contains an implementation of every operation from the generated code
+	server := api.NewServer()
+
+	r := http.NewServeMux()
+
+	// get an `http.Handler` that we can use
+	h := api.HandlerFromMux(server, r)
+
+	s := &http.Server{
+		Handler: h,
+		Addr:    "0.0.0.0:8080",
+	}
+
+	// And we serve HTTP until the world ends.
+	log.Fatal(s.ListenAndServe())
+}
