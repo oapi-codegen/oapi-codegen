6fd51d63a72f8e8e17907557e2ba6ac30364c9db
Viktoras Makauskas <viktoras@neglostyti.com>
2025-03-15 15:16:41 +0200

    wip

--- pkg/util/loader_test.go
+++ pkg/util/loader_test.go
@@ -1,33 +1,46 @@
 package util
 
 import (
+	"bytes"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 	"testing"
 )
 
 func TestYamlMarshal(t *testing.T) {
-	r := require.New(t)
-
-	t.Run("check that yaml.v3 workaround for multiline strings with leading spaces still works", func(t *testing.T) {
-		spec := openapi3.T{}
-		spec.AddOperation("info", "GET", &openapi3.Operation{
-			Summary: "Get info",
-			Parameters: openapi3.Parameters{
-				{
-					Value: &openapi3.Parameter{
-						Description: "     a\nb",
-					},
+	spec := openapi3.T{}
+	spec.AddOperation("info", "GET", &openapi3.Operation{
+		Summary: "Get info",
+		Parameters: openapi3.Parameters{
+			{
+				Value: &openapi3.Parameter{
+					Name:        "name",
+					Description: " a\n b",
 				},
 			},
-		})
-		spec.Paths.Extensions = map[string]interface{}{}
+		},
+	})
+	spec.Paths.Extensions = map[string]interface{}{}
 
+	t.Run("check marshal/unmarshal via workaround: should not modify spec", func(t *testing.T) {
+		r := require.New(t)
 		contents, err := yamlMarshal(&spec)
 		r.NoError(err)
 
 		unmarshalledSpec, err := openapi3.NewLoader().LoadFromData(contents)
 		r.NoError(err)
 		r.Equal(spec, *unmarshalledSpec)
+	})
+
+	t.Run("check that yaml.v3 bug is still present and needs a workaround", func(t *testing.T) {
+		r := require.New(t)
+		contents, err := yaml.Marshal(&spec)
+		r.NoError(err)
+
+		var node yaml.Node
+		err = yaml.NewDecoder(bytes.NewReader(contents)).Decode(&node)
+		r.Error(err, "expected this to fail due to a bug in go-yaml.v3 library. if it's no longer here, maybe the workaround is no longer needed.")
+		r.EqualError(err, "yaml: line 6: did not find expected key")
 	})
 }
