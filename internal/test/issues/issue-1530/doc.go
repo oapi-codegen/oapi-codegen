d724d2f58fe8532ac95f443860b7672c041d24ac
Matthew Gabeler-Lee <matt.gabeler-lee@ocado.com>
2025-03-27 09:38:09 -0400

    fix: add another test case from champtar, but not failing yet

--- internal/test/issues/issue-1530/doc.go
+++ internal/test/issues/issue-1530/doc.go
@@ -1,3 +1,4 @@
 package param
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml issue1530.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config-1.yaml issue1530-1.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config-2.yaml issue1530-2.yaml
