67d8c4607f03785861572644c948b766a7a2f52c
Jamie Tanna <jamie@jamietanna.co.uk>
2025-05-07 08:05:54 +0100

    chore(tests): add test case for `x-go-type` and `-skip-optional-pointer`
    As part of future changes, we'll be modifying the way this works, so we
    should capture the current behaviour.

---
+++ internal/test/issues/issue1957/issue1957_test.go
@@ -0,0 +1,58 @@
+package issue1957
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGeneratedCode(t *testing.T) {
+	t.Run("For an object", func(t *testing.T) {
+		t.Run("A required field should be a non-pointer", func(t *testing.T) {
+			theType := TypeWithOptionalField{
+				AtRequired: uuid.New(),
+			}
+
+			require.NotZero(t, theType.AtRequired)
+		})
+
+		t.Run("An optional field with x-go-type-skip-optional-pointer should be a non-pointer", func(t *testing.T) {
+			theType := TypeWithOptionalField{
+				AtRequired: uuid.New(),
+			}
+
+			require.NotZero(t, theType.AtRequired)
+		})
+	})
+
+	t.Run("For a query parameter", func(t *testing.T) {
+		// TODO that this is NOT wanted behaviour, but it is our current behaviour
+		t.Run("An optional field with x-go-type-skip-optional-pointer should be a pointer", func(t *testing.T) {
+
+			u := uuid.New()
+
+			theType := GetRootParams{
+				At: &u,
+			}
+
+			require.NotNil(t, theType.At)
+			require.NotZero(t, *theType.At)
+		})
+	})
+
+	t.Run("For a field with an AllOf", func(t *testing.T) {
+		// TODO that this is NOT wanted behaviour, but it is our current behaviour
+		t.Run("An optional field with x-go-type-skip-optional-pointer should be a pointer", func(t *testing.T) {
+
+			u := uuid.New()
+
+			theType := TypeWithAllOf{
+				Id: &u,
+			}
+
+			require.NotNil(t, theType.Id)
+			require.NotZero(t, *theType.Id)
+		})
+	})
+}
